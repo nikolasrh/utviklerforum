aebfd3a936c1e96a21387d896db59791d094a008
Nikolas <nikolas.hugsted@bekk.no>
2022-08-23 16:53:30 +0200

    remove unused files

--- app/src/main/kotlin/Operators.kt
+++
@@ -1,6 +0,0 @@
-// package nikolasrh.utviklerforum
-
-// infix fun <A, B> Result<A>.bind(f: (A) -> Result<B>) = bind(this, f)
-
-// // infix fun <A, B, C> ((B) -> C).compose(f: ((A) -> B)) = compose(this, f)
-// infix fun <A, B, C> ((B) -> C).compose(f: ((A) -> B)): (A) -> C = { x: A -> this(f(x)) }
